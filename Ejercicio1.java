b2a14a74ad531e2de75531ddd59a627ac816c1ef
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-11 20:53:56 +0200

    first commit

---
+++ Ejercicio1.java
@@ -0,0 +1,14 @@
+public class Ejercicio1{
+    public static void main(String[] args) {
+        int x = 144;
+        int y = 999;
+        int suma = x + y;
+        int resta = x-y;
+        int division = x/y;
+        int multiplicacion = x*y;
+        System.out.println(resta);
+        System.out.println(suma);
+        System.out.println(division);
+        System.out.println(multiplicacion);
+    }
+}
