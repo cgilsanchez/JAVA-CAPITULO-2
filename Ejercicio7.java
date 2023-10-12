77f1491ed52b4f7d677f03c021a6925193cc0284
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-12 18:26:37 +0200

    first commit

---
+++ Ejercicio7.java
@@ -0,0 +1,7 @@
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        char letra = 'a';
+        String palabra = "Carlos";
+        System.out.println(letra + " " +palabra);
+    }
+}
