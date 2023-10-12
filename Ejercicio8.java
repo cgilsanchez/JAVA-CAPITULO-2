a60165e85e371a01992674834f8b9dff8e9a0eb9
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-12 18:26:46 +0200

    first commit

---
+++ Ejercicio8.java
@@ -0,0 +1,11 @@
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        char letra1 = 'a';
+        char letra2 = 'b';
+        char letra3 = 'c';
+        char letra4 = 'd';
+        char letra5 = 'e';
+        String concatena = " ";
+            System.out.println(letra1 + concatena + letra2+ concatena+ letra3+ concatena+letra4+concatena+letra5);
+    }
+}
