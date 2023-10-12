d71e9f406f8d0894d52bf5a60fa8da670da68ea2
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-12 18:25:51 +0200

    first commit

---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        String nombre = "Carlos Gil Sanchez";
+        String direccion = "C/Leopoldo alas clarin";
+        long numero = 644743172;
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Direccion: "+ direccion);
+        System.out.println("Numero: "+ numero);
+    }
+}
